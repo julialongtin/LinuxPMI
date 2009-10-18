9ea07123eeeb9f87392f6a484bcaa1b4492f00c2
Ash Wiren <ash@spooksoftware.com>
2009-10-18 14:35:57 +0800

    updated mkinitrd.sh to actually work... mostly

--- 2.6/tools/initrd_builder/mkinitrd.sh
+++ 2.6/tools/initrd_builder/mkinitrd.sh
@@ -4,25 +4,41 @@ BUSYBOXVERSION=1.15.2
 GITROOT=/array/linuxpmi/linuxPMI
 OURPWD=${GITROOT}/2.6/tools/initrd_builder
 
-initrd() {
-	umount mountpoint
-	#build .img file from staging area
-	#  dd a file
-	rm initrd
-	dd if=/dev/zero of=initrd bs=1024 count=20000
+ARCH=${ARCH:-i386}
 
-	#  mkfs ext2
-	/sbin/mkfs.ext2 -F initrd
-	#  loopback mount
-	rm -rf mountpoint
+if [ "$ARCH" = "i486" ]; then
+  LIBDIRSUFFIX=
+elif [ "$ARCH" = "i686" ]; then
+  LIBDIRSUFFIX=
+elif [ "$ARCH" = "x86_64" ]; then
+  LIBDIRSUFFIX=64
+fi
+
+set -e
+
+setupinitrd() {
+	#umount mountpoint
+
+	rm initrd*
+	dd if=/dev/zero of=initrd bs=1024 count=40000
+	/sbin/mkfs.ext2 -F -m 0 -b 1024 initrd
+	rm -rf mountpoint 
 	mkdir mountpoint
-	mount -o loop initrd mountpoint/  
-	#  set permissions on staging
-	chown -R root:root staging/*
-	#  copy from staging into loopback fs
-	cp -a staging/* mountpoint/
-	#  ??other magics??
+	mount -o loop initrd mountpoint/
+	cd mountpoint
+	mkdir -p bin etc sbin lib${LIBDIRSUFFIX} proc \
+	sys tmp migtorture usr/bin var/lock var/log dev \
+	usr/sbin
+	cd ${OURPWD}
+}
+
+finishinitrd() {
+	cd mountpoint
+	chown -R root:root *
+	cd ${OURPWD}
 	umount mountpoint
+	#initrd magics?
+	gzip initrd
 }
 
 busybox() {
@@ -32,15 +48,21 @@ busybox() {
 	cd busybox-${BUSYBOXVERSION}
 	make
 	#copy to staging area
-	make CONFIG_PREFIX=${OURPWD}/staging install
+	make CONFIG_PREFIX=${OURPWD}/mountpoint/ install
 	#setuid root busybox
 	cd ${OURPWD}
+	chmod u+s mountpoint/bin/busybox
 }
 
-#libs() {
-#	#ldd the binaries?
-#	#copy libraries to staging area
-#}
+libs() {
+	#ldd the binaries?
+	lddlibs=`ldd mountpoint/migtorture/eatcpu | grep -oE /lib[A-Za-z0-9./\-]+` 
+	#copy libraries to staging area
+	for each in $lddlibs; do {
+		cp $each mountpoint/lib${LIBDIRSUFFIX}
+	} done;
+	
+}
 
 #scripts() {
 #	#copy scripts from git to staging area
@@ -51,19 +73,15 @@ migtort() {
 	cd ${GITROOT}/2.6/tools/migtorture
 	make
 	#copy to staging area
-	cp -a * ${PWD}/staging/migtorture/
+	cp -a . ${OURPWD}/mountpoint/migtorture
 	cd ${OURPWD}
 }
 
-staging() {
-	rm -rf staging/
-	mkdir -p staging/bin staging/etc staging/sbin staging/lib staging/proc \
-	staging/sys staging/tmp staging/migtorture staging/usr/bin \
-	staging/usr/sbin
-}
-
-staging
+setupinitrd
 migtort
-initrd
+busybox
+libs
+finishinitrd
+
 
 
