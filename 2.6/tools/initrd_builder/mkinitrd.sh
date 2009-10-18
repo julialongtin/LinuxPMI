baeee842a05a91709a6c6cd0be2e626cdf6108dc
Ash Wiren <ash@spooksoftware.com>
2009-10-18 17:26:31 +0800

    mkinitrd script is now finished and working.

--- 2.6/tools/initrd_builder/mkinitrd.sh
+++ 2.6/tools/initrd_builder/mkinitrd.sh
@@ -19,13 +19,24 @@ set -e
 setupinitrd() {
 	#umount mountpoint
 
+	echo "*** Deleting old initrds"
 	rm initrd*
-	dd if=/dev/zero of=initrd bs=1024 count=40000
-	/sbin/mkfs.ext2 -F -m 0 -b 1024 initrd
+
+	echo "*** Creating new initrd file"
+	dd if=/dev/zero of=initrd bs=1024 count=32000 1> /dev/null
+
+	echo "*** Formating ext2"
+	/sbin/mkfs.ext2 -F -m 0 -b 1024 initrd 1> /dev/null
+
+	echo "*** Creating mountpoint"
 	rm -rf mountpoint 
 	mkdir mountpoint
+
+	echo "*** Loopback mounting initrd"
 	mount -o loop initrd mountpoint/
 	cd mountpoint
+
+	echo "*** Making directories in our new /"
 	mkdir -p bin etc sbin lib${LIBDIRSUFFIX} proc \
 	sys tmp migtorture usr/bin var/lock var/log dev \
 	usr/sbin
@@ -33,46 +44,65 @@ setupinitrd() {
 }
 
 finishinitrd() {
+	echo "*** Copying etc files"
+	cp -a files/etc/* mountpoint/etc/
+	echo "*** Copying device files"
+	cp -a files/dev/* mountpoint/dev/
+	cp -a /dev/console /dev/kmem /dev/mem /dev/null /dev/ram0 \
+	/dev/tty1 /dev/tty2 /dev/tty5  mountpoint/dev/
+
 	cd mountpoint
+	echo "*** Chowning all files to root"
 	chown -R root:root *
 	cd ${OURPWD}
+
+#	find . | cpio --quiet -c -o | gzip > ../initrd.img
+	echo "*** umounting initrd"
+#	cd ${OURPWD}
 	umount mountpoint
 	#initrd magics?
-	gzip initrd
+	echo "*** gzipping initrd"
+	gzip -9 initrd
 }
 
 busybox() {
-	#copy config file
+	echo "*** Copying busybox config file"
 	cp bbconfig busybox-${BUSYBOXVERSION}/.config
 	#build busybox
+	echo "*** Building busybox, may take some time, up to 5 minutes"
 	cd busybox-${BUSYBOXVERSION}
-	make
+#	make clean 1> /dev/null
+	make 1> /dev/null
 	#copy to staging area
-	make CONFIG_PREFIX=${OURPWD}/mountpoint/ install
-	#setuid root busybox
+
+	echo "*** Installing busybox + symlinks"
+	make CONFIG_PREFIX=${OURPWD}/mountpoint/ install 1> /dev/null
 	cd ${OURPWD}
+	echo "*** Setuid root busybox"
 	chmod u+s mountpoint/bin/busybox
 }
 
 libs() {
 	#ldd the binaries?
+	echo "*** Getting ldd of binaries"
 	lddlibs=`ldd mountpoint/migtorture/eatcpu | grep -oE /lib[A-Za-z0-9./\-]+` 
 	#copy libraries to staging area
+	echo "*** Copying libraries to initrd"
 	for each in $lddlibs; do {
 		cp $each mountpoint/lib${LIBDIRSUFFIX}
 	} done;
 	
 }
 
-#scripts() {
-#	#copy scripts from git to staging area
-#}
-
 migtort() { 
+	echo "*** Building migtorture"
 	#build migtorture
 	cd ${GITROOT}/2.6/tools/migtorture
-	make
+
+#	make clean 1> /dev/null
+	make 1> /dev/null
 	#copy to staging area
+	echo "*** Copying migtorture to initrd"
 	cp -a . ${OURPWD}/mountpoint/migtorture
 	cd ${OURPWD}
 }
